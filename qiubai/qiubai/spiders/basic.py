ab3a040f45b6995f5da18120a056f8cee6696100
Chris <coding.net>
2016-07-26 18:44:28 +0800

    pull?

--- qiubai/qiubai/spiders/basic.py
+++ qiubai/qiubai/spiders/basic.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import scrapy
 from qiubai.items import QiuBaiItem
+import time
 class QiuBaiSpider(scrapy.Spider):
     name = "qiubai"
     start_urls = (
@@ -10,4 +11,7 @@ class QiuBaiSpider(scrapy.Spider):
     def parse(self, response):
         item = QiuBaiItem()
         item['content'] = response.xpath('//div[@class="content"]/text()').extract()
+        url = 'http://www.qiushibaike.com' + response.xpath('//span[@class="next"]/parent::*/@href').extract_first()
+        time.sleep(1)
         yield item
+        yield scrapy.Request(url,callback=self.parse)
