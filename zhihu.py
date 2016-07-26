7f870e43eb44345ff36f19db87f472606cc828cf
chenpengwuli <chenpengwuli@163.com>
2016-07-26 10:25:12 +0800

    第二个版本的ZHIHU spider
    这个版本将爬虫封装为一个类，方便爬取，但目前只使用到第二页，后续改进

--- zhihu.py
+++ zhihu.py
@@ -1,58 +1,100 @@
 # -*- coding: utf-8 -*-
 import sys
 reload(sys)
-sys.setdefaultencoding('utf-8') #解决中文字符的问题
+sys.setdefaultencoding('utf-8')
 import pycurl
 import StringIO
 import urllib
 from lxml.etree import HTML
 from lxml import etree
-phone_num = 'xxx' 
-password = 'xxx'
-url = 'http://www.zhihu.com/login/phone_num' #这个地址是zhihu手机号登录的post地址
-data = {'phone_num':phone_num,'password':password} #这个是Post的form信息
-after_login = 'http://www.zhihu.com/#signin' #登录后再次打开首页可获得信息
-def login_zhihu(data,url):
-	c = pycurl.Curl()
-	str1 = StringIO.StringIO()
-	c.setopt(pycurl.URL,url)
-	c.setopt(c.WRITEFUNCTION,str1.write)
-	# c.setopt(pycurl.VERBOSE,1)
-	c.setopt(pycurl.POSTFIELDS,urllib.urlencode(data)) #Pycurl的post方法
-	c.setopt(pycurl.FOLLOWLOCATION,1)
-	c.setopt(pycurl.MAXREDIRS,5)
-	c.setopt(pycurl.COOKIEFILE,'') #使用cookie?
-	c.perform() 
-	print str1.getvalue() #这里打印登录后的response，为一个Json，成功则为0
+import json
+class zhihu(object):
+	c = None
+	def __init__(self):
+		self.c = pycurl.Curl()
+		self.login_url ='http://www.zhihu.com/login/phone_num'
+		self.after_login_url ='http://www.zhihu.com/#signin'
+		self.feedlist_url = 'https://www.zhihu.com/node/TopStory2FeedList'
+		self.phone_num = 'xx'
+		self.password = 'xx'
+		self.data = {'phone_num':self.phone_num,'password':self.password}
+		
+	def login(self): #login the zhihu 
+		buff = StringIO.StringIO()
+		self.c.setopt(pycurl.URL,self.login_url)
+		self.c.setopt(pycurl.FOLLOWLOCATION,1)		
+		self.c.setopt(pycurl.MAXREDIRS,5)
+		self.c.setopt(pycurl.COOKIEFILE,'temp')
+		# self.c.setopt(pycurl.VERBOSE,1)
+		self.c.setopt(pycurl.POSTFIELDS,urllib.urlencode(self.data))
+		self.c.setopt(self.c.WRITEFUNCTION,buff.write)
+		self.c.perform()
+		print buff.getvalue()
+		# print self.data
+	def homepage(self): # after login,retrieve the homepage contents of zhihu
+		buff = StringIO.StringIO()
+		self.c.setopt(pycurl.URL,self.after_login_url)
+		self.c.setopt(pycurl.FOLLOWLOCATION,1)			
+		self.c.setopt(pycurl.MAXREDIRS,5)	
+		self.c.setopt(pycurl.COOKIEFILE,'temp')
+		self.c.setopt(pycurl.HTTPGET,1)
+		self.c.setopt(self.c.WRITEFUNCTION,buff.write)
+		self.c.perform()
+		self.parse_xsrftoken(buff.getvalue())
+		self.parse_content(buff.getvalue()) #call the function parse_content to parse the zhihu contents
+		# print 'here2'
+	def parse_xsrftoken(self,contents):
+		html=HTML(contents)
+		self.xsrftoken = html.xpath('.//input[@name="_xsrf"]/@value')[0]
+	def nextcontents(self,page='2'): # then retrieve the next 10 contents of zhihu
+		buff = StringIO.StringIO()
+		params = {"offset":10,"start":'9'}
+		method = "next"
+		data_feed ={'params':params,'method':method}
+		header =['Accept:*/*',
+				'Accept-Language:zh-CN,zh;q=0.8',
+				'Connection:keep-alive',
+				'Content-Type:application/x-www-form-urlencoded; charset=UTF-8',
+				'Origin:https://www.zhihu.com',
+				'Referer:https://www.zhihu.com/',
+				'X-Requested-With:XMLHttpRequest',
+				'X-Xsrftoken:'+self.xsrftoken]
+		self.c.setopt(pycurl.URL,self.feedlist_url)		
+		self.c.setopt(pycurl.FOLLOWLOCATION,1)		
+		self.c.setopt(pycurl.MAXREDIRS,5)
+		self.c.setopt(pycurl.HTTPHEADER,header)
+		self.c.setopt(pycurl.POSTFIELDS,urllib.urlencode(data_feed).replace('%27','%22').replace('+',''))
+		self.c.setopt(self.c.WRITEFUNCTION,buff.write)
+		self.c.perform()
+		contents = json.loads(buff.getvalue())['msg']
+		for content in contents:
+			self.parse_content(content)
+		
+	def save_to_file(self,filename):# save the retrieved contents and save them to file
+		pass
 
-	c.setopt(pycurl.URL,after_login) #打开首面
-	c.setopt(pycurl.HTTPGET,1) #使用get方法
-	c.setopt(c.WRITEFUNCTION,str1.write) 
-	c.perform()
-	html = HTML(str1.getvalue()) #使用HTML将文本转为html类型
-	pages = html.xpath('.//div[@class="feed-content"]') #对于title:content类型的结构，先取到它的Parent div部分，再依取得各文本
-	for page in pages:
-		page = HTML(etree.tostring(page))
-		title = page.xpath('.//h2[@class="feed-title"]/a/text()')
-		content = page.xpath('.//div[@class="zh-summary summary clearfix"]/text()')
+	def parse_content(self,contents):
+		html=HTML(contents)
+	# print xsrftoken
+		pages = html.xpath('.//div[@class="feed-content"]')
+		for page in pages:
+			page = HTML(etree.tostring(page))
+			title = page.xpath('.//h2[@class="feed-title"]/a/text()')
+			content = page.xpath('.//div[@class="zh-summary summary clearfix"]/text()')
 
-		if title:
-			for tit in title:
-				print tit.strip()
-		# print len(tit)
-		if content:
-			for con in content:
-				print con.strip()
-		# print len(content)
-		# print etree.tostring(page)
-		# contents = dict(zip(titles,pages))
-	# for page in pages:
-	# 	print page.strip()
-	# for title in titles:
-	# 	print title
-	# for (key,value) in content.items():
-	# 	print '[',key,':',value,']'
-	c.close()
+			if title:
+				for tit in title:
+					print tit.strip()
+
+			if content:
+				for con in content:
+					print con.strip()
+	def run(self):
+		self.login()
+		self.homepage()
+		self.nextcontents()
+		self.c.close()
 
 if __name__=='__main__':
-	login_zhihu(data,url)
+	zhspider = zhihu()
+	zhspider.run()
